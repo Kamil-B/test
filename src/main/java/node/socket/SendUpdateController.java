0c449a3a19c6605ef3f04193f66342843dddf3ba
Kamil Bednarczyk <kamil.bednarczyk@connectis.pl>
2018-03-14 19:07:53 +0100

    implementation of websocket tutorial

--- src/main/java/node/socket/SendUpdateController.java
+++ src/main/java/node/socket/SendUpdateController.java
@@ -1,17 +1,34 @@
 package node.socket;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
 
-@RestController
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+@EnableScheduling
 public class SendUpdateController {
 
     //https://medium.com/oril/spring-boot-websockets-angular-5-f2f4b1c14cee
 
-    @MessageMapping("/user")
-    @SendTo("/topic/user")
-    public String send(){
-        return "TEST";
+    @Autowired
+    private SimpMessagingTemplate template;
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Message greeting(Message message){
+        return  new Message("Welcome" + message);
+    }
+
+    @Scheduled(fixedDelay = 1000)
+    public void sendAdhocMessages() {
+        template.convertAndSend("/topic/greetings", new Message("Fixed Delay Scheduler"));
     }
 }
--- src/main/java/node/socket/WebSocketConfig.java
+++ src/main/java/node/socket/WebSocketConfig.java
@@ -11,14 +11,13 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket").setAllowedOrigins("*").withSockJS();
-
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
     }
 }
